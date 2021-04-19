9b77ea22b6c4c4956224eaa84dd42a89c74497a1
sakkarakaya <mehmet-sakib.karakaya@stud.hs-hannover.de>
2021-04-19 17:38:37 +0200

    done

---
+++ index.js
@@ -0,0 +1,41 @@
+class Calculator{
+    constructor(prevOperand, currOperand){
+        this.previousOperand = prevOperand;
+        this.currentOperand = currOperand;
+    }
+    clear(){
+
+    }
+    delete(){
+
+    }
+    appendNumber(number){
+        console.log(number)
+    }
+    chooseOperation(){
+
+    }
+    compute(){
+
+    }
+    getDisplayNumber(){
+
+    }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-del]');
+const clearButton = document.querySelector('[data-all-clear]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", function(){
+        calculator.appendNumber(button.innerText);
+    })
+})
