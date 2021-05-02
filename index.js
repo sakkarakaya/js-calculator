52a658ac9a52da59944ff29ef7e424051ad3b5be
sakkarakaya <sakkarakaya@icloud.com>
2021-05-02 14:44:11 +0200

    done

--- index.js
+++ index.js
@@ -2,24 +2,57 @@ class Calculator{
     constructor(prevOperand, currOperand){
         this.previousOperand = prevOperand;
         this.currentOperand = currOperand;
+        this.clear()
     }
     clear(){
-
+        this.cOperand = '';
+        this.pOperand = '';
+        this.operation = undefined;
     }
     delete(){
-
+        this.cOperand = this.cOperand.toString().slice(0, -1);
     }
     appendNumber(number){
-        
+        if(number === '.' && this.cOperand.includes('.')) return
+        this.cOperand = this.cOperand.toString() + number.toString()
     }
-    chooseOperation(){
-
+    chooseOperation(operation){
+        this.operation = operation;
+        this.pOperand = this.cOperand;
+        this.cOperand = ''
     }
     compute(){
-
+        let result
+        switch(this.operation){
+            case "+":
+                result = parseInt(this.cOperand) + parseInt(this.pOperand)
+                break
+            case "-":
+                result = parseInt(this.pOperand) - parseInt(this.cOperand)
+                break
+            case "/":
+                result = parseInt(this.pOperand) / parseInt(this.cOperand)
+                break
+            case "*":
+                result = parseInt(this.cOperand) * parseInt(this.pOperand)
+                break
+            default:
+                return
+        }
+        this.cOperand = result;
+        this.pOperand = '';
+        this.operation = undefined
     }
-    getDisplayNumber(){
-
+    
+    updateDisplay(){
+        this.currentOperand.innerText = this.cOperand
+        if(this.operation != null){
+            this.previousOperand.innerText = this.pOperand + this.operation
+        }
+        else{
+            this.previousOperand.innerText = '';
+        }
+        
     }
 }
 
@@ -37,6 +70,30 @@ const calculator = new Calculator(previousOperandText, currentOperandText);
 numberButtons.forEach((btn) => {
     btn.addEventListener("click", function(){
         calculator.appendNumber(btn.innerText);
+        calculator.updateDisplay()
     })
 })
+
+operationButtons.forEach((btn) => {
+    btn.addEventListener("click", function(){
+        calculator.chooseOperation(btn.innerText);
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener("click", function(){
+        calculator.compute();
+        calculator.updateDisplay()
+    })
+
+deleteButton.addEventListener("click", function(){
+        calculator.delete();
+        calculator.updateDisplay()
+    })
+
+clearButton.addEventListener("click", function(){
+        calculator.clear();
+        calculator.updateDisplay()
+    })
+
 
